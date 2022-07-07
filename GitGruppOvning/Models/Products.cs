f9558098899bc5f59187003072c456b09e658f8c
Melikinho <Jimmymelik10@gmail.com>
2022-07-07 10:54:02 +0200

    Uppdateringar i products

--- GitGruppOvning/Models/Products.cs
+++ GitGruppOvning/Models/Products.cs
@@ -5,8 +5,11 @@ public class Products
     public int id { get; set; }
     public string name { get; set; }
     public double price { get; set; }
-    private bool _disposed = false}
     private int key { get; set; }
-    protected int lastName { get; set; }
+    protected string lastName { get; set; }
+
+    public int intNumber { get; set; }
+    
+    
 
 }
